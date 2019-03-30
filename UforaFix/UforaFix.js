beac8377ebeee31f04aaadfcd7319769c591716a
Jotra04 <48770470+Jotra04@users.noreply.github.com>
2019-03-30 08:34:39 +0100

    Initialization

---
+++ UforaFix/UforaFix.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("d2l-page-main", "d2l-max-width")[0].classList.remove("d2l-max-width");
+document.getElementsByClassName("d2l-header-bottom-box-responsive")[0].remove();
+document.body.getElementsByClassName("d2l-navigation-header-left")[0].parentElement.remove();
